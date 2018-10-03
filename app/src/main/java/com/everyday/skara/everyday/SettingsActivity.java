8eb5164184271d264133f9342ca60a348ae4a4ba
shubham <shubhamvishnu@gmail.com>
2018-10-03 19:22:00 +0530

    -settings dialog - todo.

--- app/src/main/java/com/everyday/skara/everyday/SettingsActivity.java
+++ app/src/main/java/com/everyday/skara/everyday/SettingsActivity.java
@@ -1,13 +1,53 @@
 package com.everyday.skara.everyday;
 
+import android.support.design.widget.BottomSheetDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
-public class SettingsActivity extends AppCompatActivity {
+import com.everyday.skara.everyday.classes.DateTimeStamp;
+
+import org.w3c.dom.Text;
+
+public class SettingsActivity extends AppCompatActivity implements View.OnClickListener {
+    Button mSetSymbolButton;
+    TextView mSymbolTextView;
+    BottomSheetDialog mSymbolDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        init();
     }
+
+    void init() {
+        mSetSymbolButton = findViewById(R.id.set_symbol_button);
+        mSymbolTextView = findViewById(R.id.symbol_choice_textview);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.set_symbol_button:
+                showSymbolsDialog();
+                break;
+        }
+    }
+
+    void showSymbolsDialog() {
+        mSymbolDialog = new BottomSheetDialog(SettingsActivity.this);
+        mSymbolDialog.setContentView(R.layout.dialog_symbols_dialog);
+        ImageButton mClose = mSymbolDialog.findViewById(R.id.close_symbols_dialog);
+
+
+        mSymbolDialog.setCanceledOnTouchOutside(false);
+        mSymbolDialog.show();
+    }
+
 }
